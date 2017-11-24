18f34f266d8905da77860c3d9fe2b19e4cd0940b
Malgorzata Kudlak <m.kudlak@icloud.com>
2017-11-24 11:52:08 +0100

    Change in js add prompt

--- workspace/js/scripts.js
+++ workspace/js/scripts.js
@@ -1,5 +1,6 @@
 var a = 2,
     h = 5,
-    triangleArea = a*h/2;
+    triangleArea = a * h / 2;
 
     console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+    prompt (triangleArea);
