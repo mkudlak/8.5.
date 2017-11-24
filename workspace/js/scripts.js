194a0e16ec9e9095d183b7c1cdad38ece6b3747d
Malgorzata Kudlak <m.kudlak@icloud.com>
2017-11-24 12:08:07 +0100

    Delete prompt

--- workspace/js/scripts.js
+++ workspace/js/scripts.js
@@ -1,6 +1,5 @@
 var a = 2,
-    h = 5,
-    triangleArea = a * h / 2;
+h = 5,
+triangleArea = a * h / 2;
 
-    console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
-    prompt (triangleArea);
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
