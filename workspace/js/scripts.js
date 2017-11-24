24f96206c62a6c8d2b2738bb32d4e60839d63ead
Malgorzata Kudlak <m.kudlak@icloud.com>
2017-11-24 11:38:32 +0100

    Change in js

--- workspace/js/scripts.js
+++ workspace/js/scripts.js
@@ -1,3 +1,5 @@
-var name = prompt('Podaj swoje imie nieznajomy');
-alert('Witaj, ' + name);
-console.log('Hello, ' + name);
+var a = 2,
+    h = 5,
+    triangleArea = a*h/2;
+
+    console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
